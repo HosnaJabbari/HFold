488a313e34ec422cebd94c77bcdeb8ac306bfb8c
IanWark <wark@ualberta.ca>
2017-07-28 16:04:38 -0600

    Allocate V final based on sequence length not MAXSLEN

--- V_final.cpp
+++ V_final.cpp
@@ -8,25 +8,25 @@
 #include "V_final.h"
 #include "h_struct.h"
 
-V_final::V_final(){
-	index = new int [MAXSLEN];
-    int total_length = (MAXSLEN *(MAXSLEN+1))/2;
+V_final::V_final(int nb_nucleotides){
+	index = new int [nb_nucleotides];
+    int total_length = (nb_nucleotides *(nb_nucleotides+1))/2;
     index[0] = 0;
     int i;
-    for (int i=1; i < MAXSLEN; i++)
-        index[i] = index[i-1]+MAXSLEN-i+1;
-    
+    for (int i=1; i < nb_nucleotides; i++)
+        index[i] = index[i-1]+nb_nucleotides-i+1;
+
     type = new int[total_length];
     if (type == NULL) giveup ("Cannot allocate memory", "V_final");
     for (i = 0; i < total_length; i++) type[i] = -1;
 //	printf("an object of V_final was successfully created! \n");
 
-	
+
 }
 
 V_final::~V_final(){
 	delete [] index;
-	delete [] type;	
+	delete [] type;
 }
 
 void V_final::setloops(s_energy_matrix *v, VM_final *vm){
@@ -40,8 +40,8 @@ int V_final::get_energy(int i, int j){
 	// Hosna: June 28th, 2007
 	if (i >= j || (fres[i].pair > -1 && fres[i].pair != j) || (fres[j].pair > -1 && fres[j].pair != i)){
 		return INF;
-	}	
-	
+	}
+
 	int v_energy = v->get_energy(i,j);
 	/*if (i>=13 && j<=39 && v_energy<INF){
 	printf("V_final: v_energy(%d,%d) = %d and type = %c\n", i,j,v_energy, get_type(i,j));
@@ -54,7 +54,7 @@ int V_final::get_energy(int i, int j){
 	}else{
 		type[ij] = 1;
 	}
-	return MIN(v_energy,vm_energy);	
+	return MIN(v_energy,vm_energy);
 }
 
 char V_final::get_type(int i, int j){
--- V_final.h
+++ V_final.h
@@ -7,25 +7,25 @@
 
 class VM_final;
 class V_final{
-	public: 
+	public:
 	// constructor
-	V_final();
+	V_final(int nb_nucleotides);
 	~V_final();
 	void setloops(s_energy_matrix *v, VM_final *vm);
 	int get_energy(int i, int j);
-	
+
 	char get_type (int i, int j);
     // return the type at v_final(i,j)
-    
+
     void set_features(str_features *f){fres = f;}
-		
+
 	protected:
 	s_energy_matrix *v;
 	VM_final *vm;
 	int *index;
 	int *type;
 	str_features *fres;
-	
-	
+
+
 };
 #endif /*V_FINAL_H_*/
